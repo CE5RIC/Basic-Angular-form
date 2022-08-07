9a1a80b4a4a427cb4c5f151a0faeefc93b18c790
CE5RIC <cedricdierckx5@hotmail.com>
2022-08-07 20:55:57 +0200

    language array as per typescript syntax

--- angular-form/src/app/app.component.ts
+++ angular-form/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-form';
-  languages = ['html', 'css', 'javascript', 'php'];
+  languages:Array<string> = ['HTML', 'JS', 'PHP', 'TypeScript'];
 }
 
 
